5957d1c3e8ff3001211e2918b68c18811fba6a65
xxd <826795224@qq.com>
2019-11-19 22:18:39 +0800

    代码格式调整

---
+++ web_back/js/utils/user.js
@@ -0,0 +1,30 @@
+var baseUrl = 'http://localhost:8000/'
+
+
+var user = {
+    logout: function (callback) {
+        // 2. 调用接口
+        $.post(baseUrl + 'admin/logout', function (res) {
+            callback(res);
+        })
+    },
+    login: function (userName, userPassword, callback) {
+
+        $.post(baseUrl + 'admin/login', {
+            user_name: userName,
+            password: userPassword
+        }, function (res) {
+            callback(res);
+        })
+    },
+
+    getInfo: function (callback) {
+        $.get(baseUrl + 'admin/getuser', function (res) {
+            callback(res)
+        })
+    }
+
+}
+
+
+
